0a44651026120ebc9fd2498d2aa97216bcc0ac7d
GGITS_2020_INTERNS <GGITS_2020_INTERNS@hotmail.com>
2020-05-16 18:08:41 +0530

    RR- added whitenoise

--- PhotoCombine/settings.py
+++ PhotoCombine/settings.py
@@ -127,6 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
